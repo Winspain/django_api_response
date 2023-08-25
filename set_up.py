4be340a3ace859f865e1eecf160b9fa1998cb32a
winspain <penndicardo@outlook.com>
2023-08-25 15:53:52 +0800

    feat:set up

---
+++ set_up.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_api_response',
+    version='0.1.0',
+    python_requires='>=3.8',
+    packages=find_packages(),
+    install_requires=[],
+    description='',
+    author='dicardo',
+    author_email='',
+    classifiers=[
+        'License :: Apache License Version 2.0',
+
+        'Operating System :: POSIX :: Linux',
+
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
