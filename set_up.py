b570627c0fd2666ca8387638017077908ddbb8dc
winspain <penndicardo@outlook.com>
2023-08-31 15:28:59 +0800

    feat:modify set_up.py

--- set_up.py
+++ set_up.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django_api_response',
+    name='django_useful_utils',
     version='0.1.0',
     python_requires='>=3.8',
     packages=find_packages(),
