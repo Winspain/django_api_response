a81b860032d31545d4cb801a153e75c0a5b3d38d
winspain <penndicardo@outlook.com>
2023-09-04 17:30:23 +0800

    feat

---
+++ django_useful_utils/api_response_utils.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-　　
+import json
+import time
+
+from django.conf import settings
+from django.utils.deprecation import MiddlewareMixin
+
+
+class ApiResponseUtils():
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        request.start_time = time.time()
+        try:
+            body = json.loads(request.body)
+        except Exception:
+            body = str(request.body)
+        if request.FILES:
+            body = 'file contents'
+        post = str(request.POST)
+        response = self.get_response(request)
+        execute_time = round((time.time() - request.start_time) * 1000)
+        try:
+            response_data = str(response.content, encoding='utf-8') if response.status_code != 500 else 'Please check error log'
+        except Exception as e:
+            response_data = e
+
+    def process_request(self, request):
+        pass
+
+    def process_response(self, request, response):
+        return response
