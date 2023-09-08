2dd498198a7003cadffb84d65c6b68c50a92de73
winspain <penndicardo@outlook.com>
2023-09-08 15:57:21 +0800

    fix version

--- set_up.py
+++ set_up.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_useful_utils',
-    version='0.1.0',
+    version='0.1.1',
     python_requires='>=3.8',
     packages=find_packages(),
     install_requires=[],
